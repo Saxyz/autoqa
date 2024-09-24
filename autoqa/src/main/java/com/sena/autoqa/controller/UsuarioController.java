8c5219835045496b17919b890086e29db3d850dc
Juan Sebastián Sarmiento <sarmientun@gmail.com>
2024-09-23 20:49:00 -0500

    Commit por si acaso

--- autoqa/src/main/java/com/sena/autoqa/controller/UsuarioController.java
+++ autoqa/src/main/java/com/sena/autoqa/controller/UsuarioController.java
@@ -5,10 +5,7 @@ import com.sena.autoqa.service.UsuarioService;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -38,6 +35,9 @@ public class UsuarioController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    //@PostMapping
+    //public
 
 }
 
